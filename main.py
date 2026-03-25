342bf10e79c29e9f81ac1f7fc0b8a5dda07e29cd
awyatha7711-design <awyatha7711@gmail.com>
2026-03-25 11:12:08 +0630

    Update main.py

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def run(): app.run(host='0.0.0.0', port=10000)
 # --- CONFIGURATION ---
 TOKEN = os.getenv("BOT_TOKEN")
 # မင်းရဲ့ Channel Username ကို ဒီအောက်မှာ သေချာပေါက် ပြောင်းပေးပါ (ဥပမာ "@my_channel")
-CHANNEL_ID = "@reeac-99" 
+CHANNEL_ID = "@reeac_99" 
 # ---------------------
 
 LANG, AUTH, TEXT, VOICE = range(4)
